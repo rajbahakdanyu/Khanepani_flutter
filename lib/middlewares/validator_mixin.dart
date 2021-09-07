7846ef076f895faec8add9a30bd0f43efbd281b0
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-09-07 09:59:27 +0545

    Added mixin

---
+++ lib/middlewares/validator_mixin.dart
@@ -0,0 +1,54 @@
+class ValidationMixin {
+  validateEmail(String value) {
+    String pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regExp = new RegExp(pattern);
+    if (value.length == 0) {
+      return 'Email is Required';
+    } else if (!regExp.hasMatch(value.trim())) {
+      return 'Invalid Email';
+    } else {
+      return null;
+    }
+  }
+
+  validateMobile(String value) {
+    String pattern = r'(^[0-9]*$)';
+    RegExp regExp = new RegExp(pattern);
+    if (value.length == 0) {
+      return 'Mobile number is Required';
+    } else if (value.length < 10) {
+      return 'Mobile number must 10 or more digits';
+    } else if (!regExp.hasMatch(value.trim())) {
+      return 'Mobile Number must be digits';
+    }
+    return null;
+  }
+
+  validateName(String value) {
+    String pattern = r'(^[a-zA-Z ]*$)';
+    RegExp regExp = new RegExp(pattern);
+    if (value.length == 0) {
+      return 'Name is Required';
+    } else if (!regExp.hasMatch(value.trim())) {
+      return 'Name must be a-z and A-Z';
+    }
+    return null;
+  }
+
+  validateAmount(String value) {
+    if (value.trim().isEmpty) {
+      return "Enter an amount";
+    }
+    return null;
+  }
+
+  validatePassword(String value) {
+    if (value.length == 0) {
+      return 'Password can\'t be empty';
+    } else if (value.length < 6) {
+      return 'Password must be longer than 6 characters';
+    }
+    return null;
+  }
+}
--- lib/widgets/flight_input_widget.dart
+++ lib/widgets/flight_input_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:khanepani/widgets/input_fields_widget.dart';
 
 class FlightInputWidget extends StatefulWidget {
   const FlightInputWidget({Key? key}) : super(key: key);
@@ -9,6 +8,38 @@ class FlightInputWidget extends StatefulWidget {
 }
 
 class _FlightInputWidgetState extends State<FlightInputWidget> {
+  Widget flightInputFields() {
+    return  Column(
+      children: [
+        TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(borderSide: BorderSide.none),
+            focusedBorder: OutlineInputBorder(borderSide: BorderSide.none),
+            focusColor: Colors.grey.shade200,
+            fillColor: Colors.grey.shade200,
+            filled: true,
+            prefixIcon: Icon(Icons.arrow_upward),
+            hintText: 'Eg. Kathmnadu',
+          ),
+          cursorHeight: 25,
+        ),
+        SizedBox(height: 20),
+        TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(borderSide: BorderSide.none),
+            focusedBorder: OutlineInputBorder(borderSide: BorderSide.none),
+            focusColor: Colors.grey.shade200,
+            fillColor: Colors.grey.shade200,
+            filled: true,
+            prefixIcon: Icon(Icons.arrow_downward),
+            hintText: 'Eg. Pokhara',
+          ),
+          cursorHeight: 25,
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -16,7 +47,7 @@ class _FlightInputWidgetState extends State<FlightInputWidget> {
       child: Stack(
         alignment: AlignmentDirectional.centerEnd,
         children: [
-          InputFieldWidget(),
+          flightInputFields(),
           FloatingActionButton(
             onPressed: () {},
             child: Icon(
