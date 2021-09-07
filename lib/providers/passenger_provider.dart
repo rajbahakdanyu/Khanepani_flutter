248aecff92947eb456670f4e26adcd48f0b6d089
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-09-07 14:45:59 +0545

    Added provider functions

--- lib/providers/passenger_provider.dart
+++ lib/providers/passenger_provider.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 
 class PassengerProvider extends ChangeNotifier {
-  Map<String, int> passenger = {
-    'adult': 1,
-    'child': 0,
-  };
+  Map<String, int> passenger = {};
+
+  void updatePassengers(Map<String, int> updatedList) {
+    passenger = updatedList;
+
+    notifyListeners();
+  }
+
+  void initPassengers() {
+    passenger = {
+      'adult': 1,
+      'child': 0,
+    };
+
+    notifyListeners();
+  }
 }
